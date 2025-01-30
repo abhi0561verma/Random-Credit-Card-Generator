082a7cb1e76d09f8a4ec10b2d3abb2210aa2003a
abhi0561verma <abhishekverma0561@gmail.com>
2025-01-30 16:39:52 +0530

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Index from "./pages/Index";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const queryClient = new QueryClient();
 
 const App = () => (
-  <div>
-    <Index />
-  </div>
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
 );
 
 export default App;
